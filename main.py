938829d34598ccf3a0051f3bf990b4679da177b3
ryy182 <ryy1864@gmail.com>
2024-03-13 14:40:08 +0000

    added dictionary to list of dictionaries and final report

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ def main(file):
         book = f.read()
     word_count = count_words(book)
     letter_count = count_letters(book)
-    print(letter_count)
-    
+    letter_list = letter_to_list(letter_count)
+    print(f"---Begin report of {file}---")
+    print(f"{word_count} words found in the document")
+    for letter in letter_list:
+        print(f"The '{letter['letter']}' character was found {letter['count']} times")
+    print ("---End Report---")
+
 
 def count_words(text):
     words = text.split()
@@ -24,8 +29,16 @@ def count_letters(text):
     return letters
 
 
+def sort_on(d):
+    return d["count"]
+ 
 
-
+def letter_to_list(dict):
+    letter_list = []
+    for letter in dict:
+        letter_list.append({"letter": letter, "count": dict[letter]})
+    letter_list.sort(reverse=True, key=sort_on)
+    return letter_list
 
 
 main("books/frankenstein.txt")
